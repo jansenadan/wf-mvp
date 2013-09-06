d59fdcc9bb4117ad41a11c1d3e935d54fdf14258
Julia K Szopa <julia.krysztofiak@blackbox.vc>
2013-09-06 13:53:35 -0700

    added new prices to bras

---
+++ old bands.js
@@ -0,0 +1,123 @@
+
+
+var bands = [
+		    	{
+		    		value: 28, 
+		    		start: 22.5, 
+		    		stop: 24.8,
+		    		cups: [
+		    			{value: 'b', start: 29},
+			    		{value: 'c', start: 29.9},
+			    		{value: 'd', start: 30.7},
+		    			{value: 'e', start: 31.5},
+		    			{value: 'f', start: 32.3},
+		    			{value: 'g', start: 33.1},
+		    			{value: 'h', start: 33.9},
+		    			{value: 'hh', start: 34.6},
+		    			{value: 'j', start: 35.4},
+		    			{value: 'k', start: 36.2},
+		    			{value: 'l', start: 37},
+		    			{value: 'm', start: 37.8},
+		    			{value: 'n', start: 38.6, stop: 39.4}
+			    	]
+		    	},
+		    	{
+		    		value: 30,
+		    		start: 24.8,
+		    		stop: 26.8,
+		    		cups: [
+		    			{value: 'b', start: 31.1},
+		    			{value: 'c', start: 31.9},
+		    			{value: 'd', start: 32.7},
+		    			{value: 'e', start: 33.5},
+		    			{value: 'f', start: 34.3},
+		    			{value: 'g', start: 35},
+		    			{value: 'h', start: 35.8},
+		    			{value: 'hh', start: 36.6},
+		    			{value: 'j', start: 37.4},
+		    			{value: 'k', start: 38.2},
+		    			{value: 'l', start: 39},
+		    			{value: 'm', start: 39.8, stop: 40.6}
+		    		]
+		    	},
+		    	{
+		    		value: 32,
+		    		start: 26.8,
+		    		stop: 28.7,
+		    		cups: [
+		    			{value: 'b', start: 33.1},
+		    			{value: 'c', start: 33.9},
+		    			{value: 'd', start: 34.6},
+		    			{value: 'e', start: 35.4},
+		    			{value: 'f', start: 36.2},
+		    			{value: 'g', start: 37},
+		    			{value: 'h', start: 37.8},
+		    			{value: 'hh', start: 38.6},
+		    			{value: 'j', start: 39.4},
+		    			{value: 'k', start: 40.2},
+		    			{value: 'l', start: 40.9, stop: 41.7}
+		    		]
+		    	},
+		    	{
+		    		value: 34,
+		    		start: 28.7,
+		    		stop: 30.7,
+		    		cups: [
+		    			{value: 'b', start: 34.6},
+		    			{value: 'c', start: 35.8},
+		    			{value: 'd', start: 36.6},
+		    			{value: 'e', start: 37.4},
+		    			{value: 'f', start: 38.2},
+		    			{value: 'g', start: 39},
+		    			{value: 'h', start: 39.8},
+		    			{value: 'hh', start: 40.6},
+		    			{value: 'j', start: 41.3},
+		    			{value: 'k', start: 42.1, stop: 42.9}
+		    		]
+		    	},
+		    	{
+		    		value: 36,
+		    		start: 30.7,
+		    		stop: 32.7,
+		    		cups: [
+		    			{value: 'b', start: 37},
+		    			{value: 'c', start: 37.8},
+		    			{value: 'd', start: 38.6},
+		    			{value: 'e', start: 39.4},
+		    			{value: 'f', start: 40.2},
+		    			{value: 'g', start: 40.9},
+		    			{value: 'h', start: 41.7},
+		    			{value: 'hh', start: 42.5},
+		    			{value: 'j', start: 43.3, stop: 44.1}
+		    		]
+		    	},
+		    	{
+		    		value: 38,
+		    		start: 32.7,
+		    		stop: 34.6,
+		    		cups: [
+		    			{value: 'b', start: 39},
+		    			{value: 'c', start: 39.8},
+		    			{value: 'd', start: 40.6},
+		    			{value: 'e', start: 41.3},
+		    			{value: 'f', start: 42.1},
+		    			{value: 'g', start: 42.9},
+		    			{value: 'h', start: 43.7},
+		    			{value: 'hh', start: 44.5, stop: 45.3}
+		    		]
+		    	},
+		    	{
+		    		value: 40,
+		    		start: 34.6,
+		    		stop: 36.5,
+		    		cups: [
+		    			{value: 'b', start: 40.9},
+		    			{value: 'c', start: 41.7},
+		    			{value: 'd', start: 42.5},
+		    			{value: 'e', start: 43.3},
+		    			{value: 'f', start: 44.1},
+		    			{value: 'g', start: 44.9},
+		    			{value: 'h', start: 45.7, stop: 46.5}
+		    		]
+		    	},
+			];
